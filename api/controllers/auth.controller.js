8671269a2d3d36eaffa310d90dbb7fe64678cc8c
Khai Pong Teoh <khaipongteoh@gmail.com>
2024-05-23 23:56:41 +0100

    Define API EndPoint

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+export const register =(req,res) => {
+    const {username, email, password} = req.body
+}
+
+export const login =(req,res) => {
+    //db operations
+}
+
+export const logout =(req,res) => {
+    //db operations
+}
